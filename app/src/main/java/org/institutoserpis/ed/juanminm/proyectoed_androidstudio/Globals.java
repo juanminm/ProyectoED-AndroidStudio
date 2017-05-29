f26aa1082e5336c6061274c39db3855c4c077764
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-05-29 13:00:44 +0200

    Globals, more strings and layout

---
+++ app/src/main/java/org/institutoserpis/ed/juanminm/proyectoed_androidstudio/Globals.java
@@ -0,0 +1,55 @@
+package org.institutoserpis.ed.juanminm.proyectoed_androidstudio;
+
+/**
+ * Created by mati on 29/05/17.
+ */
+
+public class Globals {
+    public static final double METER_TO_NANOMETER = 1E9;
+    public static final double METER_TO_MICROMETER = 1E6;
+    public static final double METER_TO_MILLIMETER = 1E3;
+    public static final double METER_TO_CENTIMETER = 1E2;
+    public static final double METER_TO_KILOMETER = 1E-3;
+    public static final double METER_TO_INCH = 39.3701;
+    public static final double METER_TO_FOOT = 3.28084;
+    public static final double METER_TO_YARD = 1.09361;
+    public static final double METER_TO_MILE = 6.21371E-4;
+
+    public static final double SQUAREMETER_TO_SQUARENANOMETER = 1E18;
+    public static final double SQUAREMETER_TO_SQUAREMICROMETER = 1E12;
+    public static final double SQUAREMETER_TO_SQUAREMILLIMETER = 1E6;
+    public static final double SQUAREMETER_TO_SQUARECENTIMETER = 1E4;
+    public static final double SQUAREMETER_TO_SQUAREKILOMETER = 1E-6;
+    public static final double SQUAREMETER_TO_SQUAREINCH = 1550;
+    public static final double SQUAREMETER_TO_SQUAREFOOT = 10.7639;
+    public static final double SQUAREMETER_TO_SQUAREYARD = 1.19599;
+    public static final double SQUAREMETER_TO_SQUAREMILE = 3.861E-7;
+    public static final double SQUAREMETER_TO_ACRE = 2.47105E-4;
+    public static final double SQUAREMETER_TO_HECTARE = 1E-4;
+
+    public static final double CUBICMETER_TO_CUBICNANOMETER = 1E18;
+    public static final double CUBICMETER_TO_CUBICMICROMETER = 1E12;
+    public static final double CUBICMETER_TO_CUBICMILLIMETER = 1E6;
+    public static final double CUBICMETER_TO_CUBICCENTIMETER = 1E4;
+    public static final double CUBICMETER_TO_CUBICKILOMETER = 1E-6;
+    public static final double CUBICMETER_TO_CUBICINCH = 39.3701;
+    public static final double CUBICMETER_TO_CUBICFOOT = 3.28084;
+    public static final double CUBICMETER_TO_MILLILITER = 1E6;
+    public static final double CUBICMETER_TO_LITER = 1E3;
+
+    public static final double KILOGRAM_TO_GRAM = 1E3;
+    public static final double KILOGRAM_TO_TON = 1E-3;
+    public static final double KILOGRAM_TO_OUNCE = 35.274;
+    public static final double KILOGRAM_TO_POUND = 2.20462;
+
+    public static final double SECOND_TO_NANOSECOND = 1E9;
+    public static final double SECOND_TO_MICROSECOND = 1E6;
+    public static final double SECOND_TO_MILLISECOND = 1E3;
+    public static final double SECOND_TO_MIN = 1.6666666666667E-2;
+    public static final double SECOND_TO_HOUR = 2.7777777777778E-4;
+    public static final double SECOND_TO_DAY = 1.1574E-5;
+    public static final double SECOND_TO_MONTH = 3.805175038052E-7;
+    public static final double SECOND_TO_YEAR = 3.184064394518E-8;
+
+    public static final double CELSIUS_TO_FAHRENHEIT = 33.8;
+}
--- app/src/main/java/org/institutoserpis/ed/juanminm/proyectoed_androidstudio/MainActivity.java
+++ app/src/main/java/org/institutoserpis/ed/juanminm/proyectoed_androidstudio/MainActivity.java
@@ -2,6 +2,8 @@ package org.institutoserpis.ed.juanminm.proyectoed_androidstudio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -27,42 +29,180 @@ public class MainActivity extends AppCompatActivity {
         inputValue = (EditText) findViewById(R.id.inputValue);
         outputValue = (TextView) findViewById(R.id.outputValue);
         unitSystemsSpinner = (Spinner) findViewById(R.id.unitSystemsSpinner);
-        fromUnitSystemSpinner =  (Spinner) findViewById(R.id.fromUnitSystemSpinner);
+        fromUnitSystemSpinner = (Spinner) findViewById(R.id.fromUnitSystemSpinner);
         toUnitSystemSpinner = (Spinner) findViewById(R.id.toUnitSystemSpinner);
-        convertButton = (Button) findViewById(R.id.convertValue);
 
         unitSystemsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                @Override
-                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                    Spinner sp = (Spinner) view;
-                    String s =  sp.getSelectedItem().toString();
-                    String[] a;
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Spinner sp = (Spinner) parent;
+                String s = sp.getSelectedItem().toString();
+                String[] a;
 
-                    if(s.equals(getString(R.string.length))) {
-                        a = getResources().getStringArray(R.array.lengthUnits);
-                    } else if (s.equals(getString(R.string.area))) {
-                        a = getResources().getStringArray(R.array.areaUnits);
-                    } else if (s.equals(getString(R.string.volume))) {
-                        a = getResources().getStringArray(R.array.volumeUnits);
-                    } else if (s.equals(getString(R.string.temperature))) {
-                        a = getResources().getStringArray(R.array.temperatureUnits);
-                    } else if (s.equals(getString(R.string.mass))) {
-                        a = getResources().getStringArray(R.array.massUnits);
-                    } else if (s.equals(getString(R.string.time))) {
-                        a = getResources().getStringArray(R.array.timeUnits);
-                    }
-
-                    ArrayAdapter adapter = new ArrayAdapter<String>(this,
-                            android.R.layout.simple_spinner_item, a);
-                    adapter.notifyDataSetChanged();
-                    fromUnitSystemSpinner.setAdapter(adapter);
-                    toUnitSystemSpinner.setAdapter(adapter);
+                if (s.equals(getString(R.string.length))) {
+                    a = getResources().getStringArray(R.array.lengthUnits);
+                } else if (s.equals(getString(R.string.area))) {
+                    a = getResources().getStringArray(R.array.areaUnits);
+                } else if (s.equals(getString(R.string.volume))) {
+                    a = getResources().getStringArray(R.array.volumeUnits);
+                } else if (s.equals(getString(R.string.temperature))) {
+                    a = getResources().getStringArray(R.array.temperatureUnits);
+                } else if (s.equals(getString(R.string.mass))) {
+                    a = getResources().getStringArray(R.array.massUnits);
+                } else {
+                    a = getResources().getStringArray(R.array.timeUnits);
                 }
 
-                public void onNothingSelected(AdapterView<?> parent) {
+                inputValue.setText("0");
 
+                ArrayAdapter adapter = new ArrayAdapter<String>(MainActivity.this,
+                        android.R.layout.simple_spinner_item, a);
+                adapter.notifyDataSetChanged();
+                fromUnitSystemSpinner.setAdapter(adapter);
+                toUnitSystemSpinner.setAdapter(adapter);
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        fromUnitSystemSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String in = String.valueOf(inputValue.getText());
+
+                convert(Double.valueOf(in));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        toUnitSystemSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String in = String.valueOf(inputValue.getText());
+
+                convert(Double.valueOf(in));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        inputValue.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String in = String.valueOf(inputValue.getText());
+
+                if (in.equals("null")) {
+                    inputValue.setText("0");
+                } else {
+                    convert(Double.valueOf(in));
                 }
             }
-        )
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    protected void convert(double in) {
+        double ratioFrom;
+        double ratioTo;
+        double out;
+        String system = (String) unitSystemsSpinner.getSelectedItem();
+        String fromUnit = (String) fromUnitSystemSpinner.getSelectedItem();
+        String toUnit = (String) toUnitSystemSpinner.getSelectedItem();
+
+        ratioFrom = getRatio(system, fromUnit);
+        ratioTo = getRatio(system, toUnit);
+
+        out = in * ratioTo / ratioFrom;
+
+        outputValue.setText(String.valueOf(out));
+    }
+
+    private double getRatio(String system, String unit) {
+        double ratio = 1;
+        if (system.equals(getString(R.string.length))) {
+            if (unit.equals(getString(R.string.lengthSINanometerSymbol))) {
+                ratio = Globals.METER_TO_NANOMETER;
+            } else if (unit.equals(getString(R.string.lengthSIMicrometerSymbol))) {
+                ratio = Globals.METER_TO_MICROMETER;
+            } else if (unit.equals(getString(R.string.lengthSIMillimeterSymbol))) {
+                ratio = Globals.METER_TO_MILLIMETER;
+            } else if (unit.equals(getString(R.string.lengthSICentimeterSymbol))) {
+                ratio = Globals.METER_TO_CENTIMETER;
+            } else if (unit.equals(getString(R.string.lengthSIKilometerSymbol))) {
+                ratio = Globals.METER_TO_KILOMETER;
+            } else if (unit.equals(getString(R.string.lengthImpInchSymbol))) {
+                ratio = Globals.METER_TO_INCH;
+            } else if (unit.equals(getString(R.string.lengthImpFootSymbol))) {
+                ratio = Globals.METER_TO_FOOT;
+            } else if (unit.equals(getString(R.string.lengthImpYardSymbol))) {
+                ratio = Globals.METER_TO_YARD;
+            } else if (unit.equals(getString(R.string.lengthImpMileSymbol))) {
+                ratio = Globals.METER_TO_MILE;
+            }
+        } else if (system.equals(getString(R.string.area))) {
+            if (unit.equals(getString(R.string.areaSISquareNanometerSymbol))) {
+                ratio = Globals.SQUAREMETER_TO_SQUARENANOMETER;
+            } else if (unit.equals(getString(R.string.areaSISquareMicrometerSymbol))) {
+                ratio = Globals.SQUAREMETER_TO_SQUAREMICROMETER;
+            } else if (unit.equals(getString(R.string.areaSISquareMillimeterSymbol))) {
+                ratio = Globals.SQUAREMETER_TO_SQUAREMILLIMETER;
+            } else if (unit.equals(getString(R.string.areaSISquareCentimeterSymbol))) {
+                ratio = Globals.SQUAREMETER_TO_SQUARECENTIMETER;
+            } else if (unit.equals(getString(R.string.areaSISquareKilometerSymbol))) {
+                ratio = Globals.SQUAREMETER_TO_SQUAREKILOMETER;
+            } else if (unit.equals(getString(R.string.areaImpSquareInchSymbol))) {
+                ratio = Globals.SQUAREMETER_TO_SQUAREINCH;
+            } else if (unit.equals(getString(R.string.areaImpSquareFootSymbol))) {
+                ratio = Globals.SQUAREMETER_TO_SQUAREFOOT;
+            } else if (unit.equals(getString(R.string.areaImpSquareYardSymbol))) {
+                ratio = Globals.SQUAREMETER_TO_SQUAREYARD;
+            } else if (unit.equals(getString(R.string.areaImpSquareMileSymbol))) {
+                ratio = Globals.SQUAREMETER_TO_SQUAREMILE;
+            } else if (unit.equals(getString(R.string.areaImpSquareAcreSymbol))) {
+                ratio = Globals.SQUAREMETER_TO_ACRE;
+            } else if (unit.equals(getString(R.string.areaImpSquareHectareSymbol))) {
+                ratio = Globals.SQUAREMETER_TO_HECTARE;
+            }
+        } else if (system.equals(getString(R.string.volume))) {
+            if (unit.equals(getString(R.string.volumeSICubicNanometerSymbol))) {
+                ratio = Globals.CUBICMETER_TO_CUBICNANOMETER;
+            } else if (unit.equals(getString(R.string.volume))) {
+                ratio = Globals.CUBICMETER_TO_CUBICMICROMETER;
+            } else if (unit.equals(getString(R.string.volume))) {
+                ratio = Globals.CUBICMETER_TO_CUBICMILLIMETER;
+            } else if (unit.equals(getString(R.string.volume))) {
+                ratio = Globals.CUBICMETER_TO_CUBICCENTIMETER;
+            } else if (unit.equals(getString(R.string.volume))) {
+                ratio = Globals.CUBICMETER_TO_CUBICKILOMETER;
+            } else if (unit.equals(getString(R.string.volume))) {
+                ratio = Globals.CUBICMETER_TO_CUBICINCH;
+            } else if (unit.equals(getString(R.string.volume))) {
+                ratio = Globals.CUBICMETER_TO_CUBICFOOT;
+            } else if (unit.equals(getString(R.string.volume))) {
+                ratio = Globals.CUBICMETER_TO_MILLILITER;
+            } else if (unit.equals(getString(R.string.volume))) {
+                ratio = Globals.CUBICMETER_TO_LITER;
+            }
+        } else if (system.equals(getString(R.string.mass))) {
+        } else if (system.equals(getString(R.string.temperature))) {
+        } else if (system.equals(getString(R.string.temperature))) {
+        }
+        return ratio;
     }
 }
